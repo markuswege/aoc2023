00fe1dda97a57c3bee1bc2faf31bb774558d08c7
Markus Wege <markus.wege@outlook.de>
2023-12-04 11:24:59 +0100

    day 1 solution

---
+++ day_1/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs::{self};
+
+fn main() {
+    let file = "data.txt";
+
+    let sum: u32 = fs::read_to_string(file)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .map(|line| calculate_line(line))
+        .sum();
+
+    println!("{}", sum);
+}
+
+fn calculate_line(line: String) -> u32 {
+    let result: Vec<u32> = line
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    result.first().unwrap() * 10 + result.last().unwrap()
+}
